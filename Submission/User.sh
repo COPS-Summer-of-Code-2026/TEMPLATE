9e3f747b2a61b203ff5f54450314fb860d6b2386
Jayesh Puri <jayeshkpuri@gmail.com>
2026-05-14 20:09:06 +0530

    participant details

---
+++ Submission/User.sh
@@ -0,0 +1,9 @@
+#Participant to enter his details here:
+
+name="name goes here"
+Roll_no="roll number goes here"
+branch="branch goes here"
+remarks="remarks go here"
+#enter anything you want to add
+
+echo $name " | " $Roll_no " | " $branch " | " $remarks
