fc8a0dcd0fe6ad92d1c562d0d97acd2c4cf01454
Jayesh Puri <jayeshkpuri@gmail.com>
2026-05-16 00:57:06 +0530

    small

--- Submission/User.sh
+++ Submission/User.sh
@@ -4,7 +4,10 @@
 name="name goes here"
 Roll_no="roll number goes here"
 branch="branch goes here"
+mail="mail here @itbhu"
+phone="phone number here"
 remarks="remarks go here"
 #enter anything you want to add
 
 echo $name " | " $Roll_no " | " $branch " | " $remarks
+echo $mail - $phone
